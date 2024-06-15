997062d482c519710d0caae7fe935a9c5cd33055
sewopark <kosuking@naver.com>
2024-06-15 22:32:44 +0900

    [Feat] tmp code

--- exec/keypress.c
+++ exec/keypress.c
@@ -6,7 +6,7 @@
 /*   By: sewopark <sewopark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 18:48:21 by sewopark          #+#    #+#             */
-/*   Updated: 2024/06/15 22:04:29 by sewopark         ###   ########.fr       */
+/*   Updated: 2024/06/15 22:30:44 by sewopark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,31 +30,31 @@ int	key_press(int key, t_play *play)
 			play->player.y -= play->player.player_ray.dir_y * play->player.walk_speed;
 		printf("%c\n", play->map.field[(int)(play->player.y)][(int)(play->player.x - play->player.player_ray.dir_x * play->player.walk_speed)]);
 	}
+	//rotate to the right
+	if (key == KEY_RIGHT)
+	{
+		//both camera direction and camera plane must be rotated
+		double old_dirX = play->player.player_ray.dir_x;
+		play->player.player_ray.dir_x = play->player.player_ray.dir_x * cos(-play->player.turn_speed) - play->player.player_ray.dir_y * sin(-play->player.turn_speed);
+		play->player.player_ray.dir_y = old_dirX * sin(-play->player.turn_speed) + play->player.player_ray.dir_y * cos(-play->player.turn_speed);
+		double oldPlaneX = play->player.player_ray.plane_x;
+		play->player.player_ray.plane_x = play->player.player_ray.plane_x * cos(-play->player.turn_speed) - play->player.player_ray.plane_y * sin(-play->player.turn_speed);
+		play->player.player_ray.plane_y = oldPlaneX * sin(-play->player.turn_speed) + play->player.player_ray.plane_y * cos(-play->player.turn_speed);
+	}
+	//rotate to the left
+	if (key == KEY_LEFT)
+	{
+		//both camera direction and camera plane must be rotated
+		double old_dirX = play->player.player_ray.dir_x;
+		play->player.player_ray.dir_x = play->player.player_ray.dir_x * cos(play->player.turn_speed) - play->player.player_ray.dir_y * sin(play->player.turn_speed);
+		play->player.player_ray.dir_y = old_dirX * sin(play->player.turn_speed) + play->player.player_ray.dir_y * cos(play->player.turn_speed);
+		double oldPlaneX = play->player.player_ray.plane_x;
+		play->player.player_ray.plane_x = play->player.player_ray.plane_x * cos(play->player.turn_speed) - play->player.player_ray.plane_y * sin(play->player.turn_speed);
+		play->player.player_ray.plane_y = oldPlaneX * sin(play->player.turn_speed) + play->player.player_ray.plane_y * cos(play->player.turn_speed);
+	}
 	if (key == ESC)
 		exit_game(play);
 	return (0);
 }
 
 
-	// //rotate to the right
-	// if (key == KEY_D)
-	// {
-	// 	//both camera direction and camera plane must be rotated
-	// 	double oldplayer_ray.dir_x = player->player_ray.dir_x;
-	// 	player->player_ray.dir_x = player->player_ray.dir_x * cos(-player->turn_speed) - player->player_ray.dir_y * sin(-player->turn_speed);
-	// 	player->player_ray.dir_y = oldplayer_ray.dir_x * sin(-player->turn_speed) + player->player_ray.dir_y * cos(-player->turn_speed);
-	// 	double oldPlaneX = player->planeX;
-	// 	player->planeX = player->planeX * cos(-player->turn_speed) - player->planeY * sin(-player->turn_speed);
-	// 	player->planeY = oldPlaneX * sin(-player->turn_speed) + player->planeY * cos(-player->turn_speed);
-	// }
-	// //rotate to the left
-	// if (key == KEY_A)
-	// {
-	// 	//both camera direction and camera plane must be rotated
-	// 	double oldplayer_ray.dir_x = player->player_ray.dir_x;
-	// 	player->player_ray.dir_x = player->player_ray.dir_x * cos(player->turn_speed) - player->player_ray.dir_y * sin(player->turn_speed);
-	// 	player->player_ray.dir_y = oldplayer_ray.dir_x * sin(player->turn_speed) + player->player_ray.dir_y * cos(player->turn_speed);
-	// 	double oldPlaneX = player->planeX;
-	// 	player->planeX = player->planeX * cos(player->turn_speed) - player->planeY * sin(player->turn_speed);
-	// 	player->planeY = oldPlaneX * sin(player->turn_speed) + player->planeY * cos(player->turn_speed);
-	// }
